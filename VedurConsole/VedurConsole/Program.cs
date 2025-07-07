4e52aed38a587840480a0f08c0b4f4637e4243a9
Johann B. Kristjansson <joh4nn_bragi@hotmail.com>
2025-07-07 23:27:21 +0000

    run weatherconsole as hidden app.

--- VedurConsole/VedurConsole/Program.cs
+++ VedurConsole/VedurConsole/Program.cs
@@ -11,7 +11,9 @@ using VedurConsole.Repos;
 
 namespace VedurConsole
 {
+#pragma warning disable S1118 // Utility classes should not have public constructors
     internal class Program
+#pragma warning restore S1118 // Utility classes should not have public constructors
     {
         public static IConfigurationRoot Configuration;
 
