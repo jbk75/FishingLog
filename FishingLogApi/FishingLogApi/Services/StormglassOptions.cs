b1e36af0a0813d52672cba9333b7dfd9a40e3b6d
Johann B. Kristjansson <joh4nn_bragi@hotmail.com>
2026-01-07 22:45:08 +0000

    api

--- FishingLogApi/FishingLogApi/Services/StormglassOptions.cs
+++ FishingLogApi/FishingLogApi/Services/StormglassOptions.cs
@@ -3,5 +3,5 @@ namespace FishingLogApi.Services;
 public sealed class StormglassOptions
 {
     public string BaseUrl { get; init; } = "https://api.stormglass.io/v2/tide/extremes/point";
-    public string? ApiKey { get; init; }
+    public string? ApiKey { get; init; } = "1a08bf46-ec0d-11f0-a0d3-0242ac130003-1a08bff0-ec0d-11f0-a0d3-0242ac130003";
 }
